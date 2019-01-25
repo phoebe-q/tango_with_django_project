124ded2b9937b5487c54aa939aa5a4cbb52d564d
Phoebe Quinn <2305116q@student.gla.ac.uk>
2019-01-25 11:11:59 +0000

    Any small bugs in chapter 4 fixed, beginning chapter 5

--- rango/views.py
+++ rango/views.py
@@ -10,5 +10,9 @@ def index(request):
 
 def about(request):
     
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict ={'boldmessage': 'This tutorial has been put together by Phoebe'}
+    
+    return render(request, 'rango/about.html', context=context_dict)
+
+   # return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
 
